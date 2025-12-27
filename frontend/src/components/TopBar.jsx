e8457d3aa94948b938db4586326b3bc5aef5b74a
Kabeerul Ali <kabeerulali@gmail.com>
2025-12-27 10:57:40 +0530

    fix mime type netlify vite

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -2,9 +2,8 @@ import logo from '../../src/assets/logo.png'
 
 const  TopBar = () => {
     return <div className={'flex fles-row items-center ml-10 mb-4'}>
-        <img src={logo} alt="No img" className=' w-14 h-14 rounded-full my-4' />
-        <div className={'text-2xl ml-2 font-bold border-solid border-r-4 border-r-white pr-3 '}>Voltona</div>
-        <div className={'text-2xl pl-3 font-light'}>Creator Relationship Management</div>
+        <div className={'text-2xl ml-2 font-bold border-solid border-r-4 border-r-white pr-3 '}>FKS kabeer-corporation</div>
+        <div className={'text-2xl pl-3 font-light'}>FKS kabeer Company Member</div>
     </div>
 }
 
