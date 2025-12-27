8bf7e21279be3a9c4b734181fbd2306e4733c436
Kabeerul Ali <kabeerulali@gmail.com>
2025-12-27 10:40:10 +0530

    _redirects file added to the deploy on netify

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5174" }));
-
+app.use(cors({ origin: process.env.FRONT_ORIGIN }));
 // ✅ REQUIRED FOR FORM-DATA
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
